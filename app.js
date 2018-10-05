1f282dd805c67d89bac78371ab0cfd04321ef55d
MELVERTON\Mel <huntermelverton@gmail.com>
2018-10-05 00:35:28 -0400

    refactoring

--- app.js
+++ app.js
@@ -4,7 +4,30 @@ const yargs = require('yargs');
 
 const notes = require('./notes.js');
 
-const argv = yargs.argv;
+const titleOptions = {
+  describe: 'Title of note',
+  demand: true,
+  alias: 't'
+};
+const bodyOptions = {
+  describe: 'Body of note',
+  demand: true,
+  alias: 'b'
+};
+const argv = yargs
+  .command('add', 'add a new note', {
+    title: titleOptions,
+    body: bodyOptions
+  })
+  .command('list', 'List all notes')
+  .command('read', 'read a note', {
+    title: titleOptions
+  })
+  .command('remove', 'removing a note', {
+    title: titleOptions
+  })
+  .help()
+  .argv;
 var command = argv._[0];
 
 if (command === 'add') {
